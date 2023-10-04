71db62cf666e3e97a483cb226ef9b62f75c3025c
rnagababu75 <rnagababu75@gmail.com>
2023-10-04 21:33:26 +0530

    Added first code

---
+++ src/AreaOfTriangle.java
@@ -0,0 +1,11 @@
+public class AreaOfTriangle {
+    public static void main(String[] args)
+    {
+        int a=2;
+        int b=4;
+        double Area = 0.5*a*b;
+        System.out.println("Area_of_triangle "+ Area);
+
+
+    }
+}
